587288d57eb920bb2d4efefbcffd272b615c9021
Gurram Mohan <mohangurram700@gmail.com>
2025-06-12 10:21:47 +0530

    Updated Both Files

--- index.js
+++ index.js
@@ -5,71 +5,36 @@ var pass = "text";
 function login() {
     var userPhone = parseInt(document.getElementById("number").value);
     var userPass = document.getElementById("password").value;
-    const result = document.getElementById("res");
 
-
-
-    if(checkPhone(userPhone,(isnum)=>{
-        if(isnum){
-            checkPass(userPass,(ispass) =>{
-                if(ispass){
-                    result.style.color = "green";
-                    result.innerText = "Login successful!";
-                }
-                else{
-                    result.style.color = "orange";
-                    result.innerText = "Invalid Password";
+    checkPhone(userPhone, (isnum) => {
+        if (isnum) {
+            checkPass(userPass, (ispass) => {
+                if (ispass) {
+                    alert("Login successful!");
+                } else {
+                    alert("Invalid Password");
                 }
             });
-        }else{
-            result.style.color = "red";
-            result.innerText = "Invalid User";
-        }
-    }));
-}
-
-/*
-    if (checkPhone(userPhone)) {
-        if (checkPass(userPass)) {
-            result.style.color = "green";
-            result.innerText = "Login successful!";
         } else {
-            result.style.color = "orange";
-            result.innerText = "Invalid Password";
+            alert("Invalid User");
         }
-    } else {
-        result.style.color = "red";
-        result.innerText = "Invalid User";
-    }
-    return false;
+    });
+
+    return false; // Prevent form submission
 }
 
-
-// Check phone function
-function checkPhone(p) {
-    return p === phone;
-}
-
-// Check password function
-function checkPass(pwd) {
-    return pwd === pass;
-}
-*/
-
-function checkPhone(n,callback){
-    if(n == phone){
+function checkPhone(n, callback) {
+    if (n == phone) {
         callback(true);
-    }
-    else{
+    } else {
         callback(false);
     }
 }
 
-function checkPass(p,callback){
-    if(p == pass){
+function checkPass(p, callback) {
+    if (p == pass) {
         callback(true);
-    }
-    else{
+    } else {
         callback(false);
     }
 }
